42efcc013a27bb39bf2a768f55c47385a457b4bc
Fred6665 <43498421+Fred6665@users.noreply.github.com>
2019-10-10 20:50:52 +0300

    Update server.py

--- server.py
+++ server.py
@@ -10,10 +10,6 @@ sock.listen(1)
 
 conn, addr = sock.accept()
 
-while True:
-    data = conn.recv(1024)
-    if not data:
-        break
-    conn.send(data.upper())
+conn.send("lol".encode())
 
 conn.close()
