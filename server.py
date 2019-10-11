2a0c86e6d573d4344e848e32ffaec66467b27ae2
Fred6665 <43498421+Fred6665@users.noreply.github.com>
2019-10-11 10:59:23 +0300

    Update server.py

--- server.py
+++ server.py
@@ -14,7 +14,11 @@ print(port)
 def is_even(n):
     return n % 2 == 0
 
+def print_lol():
+    return "lol"
+
 server = SimpleXMLRPCServer((host, port))
 print("Listening on port 8000...")
 server.register_function(is_even, "is_even")
+server.register_function(print_lol, "print_lol")
 server.serve_forever()
