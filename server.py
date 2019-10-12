f2c797f6ac479210181e101e101c95c838e6cd97
Fred6665 <43498421+Fred6665@users.noreply.github.com>
2019-10-12 21:20:05 +0300

    Update server.py

--- server.py
+++ server.py
@@ -1,6 +1,7 @@
 
 import os
 import socket
+import psycopg2
 from xmlrpc.server import SimpleXMLRPCServer
 
 #socket.gethostbyname("socket22.herokuapp.com")
@@ -15,7 +16,13 @@ def is_even(n):
     return n % 2 == 0
 
 def print_lol():
-    return os.environ["DATABASE_URL"]
+    conn = psycopg2.connect(os.environ.get("DATABASE_URL"))
+    cursor = conn.cursor()
+    cursor.execute("select * from lol")
+    records =  cursor.fetchall()
+    cursor.close()
+    conn.close()
+    return records
 
 server = SimpleXMLRPCServer((host, port))
 print("Listening on port 8000...")
