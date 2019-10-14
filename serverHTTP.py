51e704c7be0fb1c41f260245bc2ce25d8c0c5ebb
Fred6665 <43498421+Fred6665@users.noreply.github.com>
2019-10-14 21:16:46 +0300

    Update serverHTTP.py

--- serverHTTP.py
+++ serverHTTP.py
@@ -1,23 +1,3 @@
-# import socket
-# port = 8000
-#
-# sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-#
-# host = socket.gethostbyname("")
-#
-# sock.bind((host,port))
-#
-#
-# sock.listen(1)
-# while True:
-#
-#     conn, addr = sock.accept()
-#
-#     conn.send("lol".encode())
-#
-#     conn.close()
-
-
 #!flask/bin/python
 from app import app
 
