3d4d88dcd9fb80891c74e73c6a4fa8c2df2de49e
Fred6665 <43498421+Fred6665@users.noreply.github.com>
2019-10-14 18:09:25 +0300

    Add files via upload

---
+++ serverHTTP.py
@@ -0,0 +1,42 @@
+# import socket
+# port = 8000
+#
+# sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+#
+# host = socket.gethostbyname("")
+#
+# sock.bind((host,port))
+#
+#
+# sock.listen(1)
+# while True:
+#
+#     conn, addr = sock.accept()
+#
+#     conn.send("lol".encode())
+#
+#     conn.close()
+
+import http.server
+import socketserver
+import os
+
+
+class MyClass(http.server.BaseHTTPRequestHandler):
+    def on_GET():
+        self.send_response(200)
+        self.send_header("Contrent-type","text/html")
+        self.end_headers()
+        self.wfile.write(cgi_directories)
+    pass
+
+
+def run(server_class=http.server.HTTPServer, handler_class= MyClass):
+    PORT = int(os.environ.get('PORT'))
+    server_address = ('', PORT)
+    httpd = server_class(server_address, handler_class)
+
+    httpd.serve_forever()
+
+if __name__ == "__main__":
+    run()
