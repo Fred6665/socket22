0e8604fdba14900c7d200ca170992d9d15925b72
Odi-117 <43498421+Odi-117@users.noreply.github.com>
2019-10-15 22:41:03 +0300

    Update app.py

--- app.py
+++ app.py
@@ -6,6 +6,11 @@ app = Flask(__name__)
 def hello_world(rrr):
     return str(rrr+100)
 
+@app.route('/events', methods=['POST'])
+def events():
+    event_data = request.json
+    return event_data[66]
+
 if __name__ == '__main__':
     PORT = int(os.environ.get('PORT'))
 
