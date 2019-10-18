50635a024ab98ce133c3a0b6f193d3bc6f43198f
Odi-117 <43498421+Odi-117@users.noreply.github.com>
2019-10-18 18:19:10 +0300

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ def error_req(error):
 @app.route('/sing_up',methods=['GET','POST'])
 def hello_world():
     #DB = os.environ.get('DATABASE_URL')
-    return "It`s GOOD!!!"
+    return str(request.get_data())
 
 @app.route('/events', methods=['POST'])
 def events():
